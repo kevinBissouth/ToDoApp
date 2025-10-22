24d5e0f84bd6a0bbbaf372ad8d19b9c32947303b
kevinBissouth <kevinbissouth237@gmail.com>
2025-10-22 14:20:52 +0100

    Fix static root configuration for Render

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -123,8 +123,12 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Si tu as aussi un dossier avec tes fichiers CSS / JS (non générés par Django)
 STATICFILES_DIRS = [
-    "/toDoApp/static/css/src",
+    os.path.join(BASE_DIR, 'static/'),  # adapte selon ton projet
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
